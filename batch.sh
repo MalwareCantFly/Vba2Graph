a1b38fc6f4e95c203cf7bbe8e35b32d6b2695749
MalwareCantFly <33734448+MalwareCantFly@users.noreply.github.com>
2020-09-13 00:04:24 +0300

    Python3 support

--- batch.sh
+++ batch.sh
@@ -5,9 +5,9 @@ mkdir olevba_output
 
 for file in input/*; do
 	echo "olevba $file"
-   	olevba -c "$file" > "olevba_output/$(basename $file).bas"
+   	olevba3 -c "$file" > "olevba_output/$(basename $file).bas"
 done
 
 for file in olevba_output/*; do
-    python vba2graph.py -i "$file" -o "output"
+    python3 vba2graph.py -i "$file" -o "output"
 done
